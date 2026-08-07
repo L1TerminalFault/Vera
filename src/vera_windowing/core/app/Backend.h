68d4e55bc0d8612f6b662be577f5bc0339ea7fbd
x <x>
2026-08-08 00:07:48 +0300

    choices enabled for x11 and wayland

--- src/vera_windowing/core/app/Backend.h
+++ src/vera_windowing/core/app/Backend.h
@@ -1,17 +1,25 @@
 #pragma once
 
+#include <cstdlib>
+
+#include "Types.h"
+
 #if defined(_WIN32)
 #include "platform/win32/Win32Backend.h"
-#else
+#elif defined(__linux__)
+#if defined(VERA_HAS_WAYLAND)
 #include "platform/wayland/WaylandBackend.hxx"
+#endif
+#if defined(VERA_HAS_X11)
 #include "platform/x11/X11Backend.hxx"
+#endif
 #endif
 
 nostd::UniquePtr<IBackend> create(const VeraAppInfo& info) {
 #if defined(_WIN32)
     (void)info;
-    return std::make_unique<Win32Backend>();
-#else
+    return nostd::makeUnique<Win32Backend>();
+#elif defined(__linux__)
     VeraLinuxProtocol protocol = info.preferedLinuxProtocol;
 
     if (protocol == VeraLinuxProtocol::Auto) {
@@ -21,17 +29,28 @@ nostd::UniquePtr<IBackend> create(const VeraAppInfo& info) {
                        : VeraLinuxProtocol::X11;
     }
 
+#if defined(VERA_HAS_WAYLAND)
     if (protocol == VeraLinuxProtocol::Wayland) {
         auto backend = nostd::makeUnique<WaylandBackend>();
         if (backend->initialize(info)) {
             return nostd::UniquePtr<IBackend>(backend.release());
         }
     }
+#endif
 
-    auto x11 = nostd::makeUnique<X11Backend>();
-    if (x11->initialize(info)) {
-        return nostd::UniquePtr<IBackend>(x11.release());
+#if defined(VERA_HAS_X11)
+    if (protocol == VeraLinuxProtocol::X11 ||
+        protocol == VeraLinuxProtocol::Auto) {
+        auto x11 = nostd::makeUnique<X11Backend>();
+        if (x11->initialize(info)) {
+            return nostd::UniquePtr<IBackend>(x11.release());
+        }
     }
+#endif
+
+    return nullptr;
+#else
+    (void)info;
     return nullptr;
 #endif
 }
